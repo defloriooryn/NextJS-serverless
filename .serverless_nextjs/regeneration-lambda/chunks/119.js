d4db41d3bed6f4d2304ab6a872f0e4c892ab4480
Deflorio Arya Nizam <deflorioaryanizam354@gmail.com>
2022-10-15 06:57:24 +0700

    add file

---
+++ .serverless_nextjs/regeneration-lambda/chunks/119.js
@@ -0,0 +1,28 @@
+"use strict";
+exports.id = 119;
+exports.ids = [119];
+exports.modules = {
+
+/***/ 7020:
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"polyfillFiles":["static/chunks/polyfills-c67a75d1b6f99dc8.js"],"devFiles":[],"ampDevFiles":[],"lowPriorityFiles":["static/N8lGsNO5ls0W3EW4JxuGd/_buildManifest.js","static/N8lGsNO5ls0W3EW4JxuGd/_ssgManifest.js"],"rootMainFiles":[],"pages":{"/":["static/chunks/webpack-9b312e20a4e32339.js","static/chunks/framework-e70c6273bfe3f237.js","static/chunks/main-8db7d21a15d54ad1.js","static/chunks/pages/index-6936c9a956518c83.js"],"/_app":["static/chunks/webpack-9b312e20a4e32339.js","static/chunks/framework-e70c6273bfe3f237.js","static/chunks/main-8db7d21a15d54ad1.js","static/chunks/pages/_app-dc14f8483464b560.js"],"/_error":["static/chunks/webpack-9b312e20a4e32339.js","static/chunks/framework-e70c6273bfe3f237.js","static/chunks/main-8db7d21a15d54ad1.js","static/chunks/pages/_error-942918685354aae9.js"]},"ampFirstPages":[]}');
+
+/***/ }),
+
+/***/ 3978:
+/***/ ((module) => {
+
+module.exports = {};
+
+/***/ }),
+
+/***/ 9450:
+/***/ ((module) => {
+
+module.exports = {"Dg":[]};
+
+/***/ })
+
+};
+;
